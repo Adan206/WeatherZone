9d73640317ded214b77008e625f40caa4bea26e0
Adan206 <adan@android-4c825ef0cdb0ba84.seattleu.edu>
2017-05-16 20:02:16 -0700

    WZ

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name weatherZoneApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the weatherZoneApp
+ */
+angular.module('weatherZoneApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
